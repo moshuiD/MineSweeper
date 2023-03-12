1edaf8929586215fa24056c6f38cc4070603c953
jx zh <2967529593@qq.com>
2023-03-12 10:08:32 +0800

    delete about form

--- resource.h
+++ resource.h
@@ -2,8 +2,6 @@
 // Microsoft Visual C++ 生成的包含文件。
 // 供 MineSweeper.rc 使用
 //
-#define IDM_ABOUTBOX                    0x0010
-#define IDD_ABOUTBOX                    100
 #define IDS_ABOUTBOX                    101
 #define IDD_MINESWEEPER_DIALOG          102
 #define IDR_MAINFRAME                   128
@@ -11,19 +9,20 @@
 #define IDR_MENU1                       138
 #define IDC_BOMBAREA                    1002
 #define ID_SETTING_EASY                 32771
-#define ID_SETTING_32772                32772
-#define ID_SETTING_32773                32773
-#define ID_SETTING_32774                32774
 #define ID_SETTING_CUSTOM               32775
 #define ID_SETTING_HARD                 32776
 #define ID_SETTING_MID                  32777
+#define IDM_SETTING_EASY                32778
+#define IDM_SETTING_MID                 32779
+#define IDM_SETTING_HARD                32780
+#define IDM_SETTING_CUSTOM              32781
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        140
-#define _APS_NEXT_COMMAND_VALUE         32778
+#define _APS_NEXT_COMMAND_VALUE         32782
 #define _APS_NEXT_CONTROL_VALUE         1004
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
